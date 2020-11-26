e6ba05473472d33d4d5420a3e272d21ed036ba46
John Reitano <john.reitano@wearesalty.com>
2020-11-25 19:07:03 -0800

    adjust script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ brew services restart redis
 
 rails new cable-ready-demo-code
 cd cable-ready-demo-code
-sed -i'' "s/gem 'tzinfo-data'/# gem 'tzinfo-data'/" Gemfile
+bundle remove tzinfo-data
 bundle add cable_ready
 yarn add cable_ready
 bundle exec rails generate channel example
@@ -22,7 +22,7 @@ cat > app/assets/stylesheets/applications.subscriptions << DONE
 
 DONE
 
-cat > app/channels/example_channel.rb <<DONE
+cat > app/channels/example_channel.rb <<"DONE"
 class ExampleChannel < ApplicationCable::Channel
   def subscribed
     stream_from "example-stream"
@@ -31,7 +31,7 @@ end
 
 DONE
 
-cat > app/javascript/channels/example_channel.js <<DONE
+cat > app/javascript/channels/example_channel.js <<"DONE"
 import CableReady from 'cable_ready'
 import consumer from './consumer'
 
@@ -44,14 +44,14 @@ consumer.subscriptions.create('ExampleChannel', {
 DONE
 
 mkdir -p app/views/home
-cat > app/views/home/index.html.erb <<DONE
+cat > app/views/home/index.html.erb <<"DONE"
 <h1>What will happen?</h1>
 
 <div id="content">Waiting...</div>
 
 DONE
 
-cat > app/controllers/home_controller.rb <<DONE
+cat > app/controllers/home_controller.rb <<"DONE"
 class HomeController < ApplicationController
   def index
     ExampleJob.set(wait: 5.seconds).perform_later
@@ -60,7 +60,7 @@ end
 
 DONE
 
-cat > config/routes.rb <<DONE
+cat > config/routes.rb <<"DONE"
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
@@ -68,7 +68,7 @@ end
 
 DONE
 
-cat > app/jobs/example_job.rb <<DONE
+cat > app/jobs/example_job.rb <<"DONE"
 class ExampleJob < ApplicationJob
   include CableReady::Broadcaster
   queue_as :default
@@ -76,18 +76,12 @@ class ExampleJob < ApplicationJob
   def perform(*args)
     cable_ready["example-stream"].inner_html(
       selector: "#content",
-      html: "Hello World, here is some big colorful text."
+      html: "<span id=\”hello\”>a big colorful hello!</span>"
     ).add_css_class(
-      selector: "#content", # required - string containing a CSS selector or XPath expression
+      selector: "#hello",
       name:     ["big", "colorful"]
     ).console_log(
-      message: "hello!"
-    ).notification(
-      title: "Some news for you...",
-      options: {
-        body: "Your app is doing well.",
-        icon: "https://source.unsplash.com/256x256"
-      }
+      message: "no bugs so far"
     )
     cable_ready.broadcast
   end
